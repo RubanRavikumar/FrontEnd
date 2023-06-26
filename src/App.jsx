cc1817dbac4f0c5082b7e2d8ec0e490a9f6c1270
RubanRavikumar <rruban006@gmail.com>
2023-06-26 20:10:15 +0530

    added some coading

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import PriceCard from './Components/PriceCard/PriceCard';
+
+function App() {
+  return(
+    <div className='App'> 
+      <PriceCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Shapes/Circle.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Circle({color=""}){
+    return <div className="Circle" style={{ background: color }}>Circle</div>
+}
